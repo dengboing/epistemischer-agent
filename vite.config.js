58846f1396f97771460c79a5e773e0f4ca773edd
dengboing <dirk.hermann@gmail.com>
2026-03-05 16:41:16 +0100

    Remove placeholder comments from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// WICHTIG: Ersetze 'epistemischer-assistent' mit deinem
-// tatsächlichen GitHub Repository-Namen!
 export default defineConfig({
   plugins: [react()],
-  
 })
